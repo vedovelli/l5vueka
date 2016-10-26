35ba5f08baccea0d1e884b1e8bb56134fd1c4a87
Fabio Vedovelli <vedovelli@gmail.com>
2016-10-26 18:58:52 +0200

    Incluidas as traduções pt_BR para o Faker. Dica do Claudio Pereira

--- database/factories/AddressModelFactory.php
+++ database/factories/AddressModelFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 $factory->define(App\Address::class, function (Faker\Generator $faker) {
+    $faker->addProvider(new Faker\Provider\pt_BR\Address($faker));
     return [
         'street' => $faker->streetName,
         'number' => $faker->randomNumber(3),
--- database/factories/UserModelFactory.php
+++ database/factories/UserModelFactory.php
@@ -2,7 +2,8 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
-
+    $faker->addProvider(new Faker\Provider\pt_BR\Person($faker));
+    $faker->addProvider(new Faker\Provider\pt_BR\Internet($faker));
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
